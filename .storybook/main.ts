fd91b318b2dc89e186f7f0d4e072aa4936fc4ac5
mar-alv <mar.alv.cod@gmail.com>
2024-03-19 20:13:40 -0300

    Issue #6  - stories for Task, TasksList and NoTasks

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,12 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
     "@chromatic-com/storybook",
+    "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-links",
+    "@storybook/addon-onboarding",
+    '@storybook/addon-themes'
   ],
   framework: {
     name: "@storybook/react-vite",
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -6,12 +6,25 @@ import { ThemeProvider } from 'styled-components';
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'light',
+          value: '#f2f2f2',
+        },
+        {
+          name: 'dark',
+          value: '#0d0d0d',
+        },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
-    },
+    }
   },
   decorators: [
     withThemeFromJSXProvider({
