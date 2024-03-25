a1d0729fcaff19650331a90aedcdbd552626e1de
mar-alv <mar.alv.cod@gmail.com>
2024-03-25 19:45:44 -0300

    Issue #5 - app tests done

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,10 +2,10 @@ module.exports = {
 	preset: 'ts-jest',
 	roots: ['<rootDir>/src'],
 	testEnvironment: 'jsdom',
-	testMatch: ['**/*.tests.tsx'],
+	testMatch: ['**/tests.tsx'],
 	moduleNameMapper: {
-		"\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
-		"\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js",
+		"\\.(css|less|sass|scss)$": "<rootDir>/.tests/styleMock.js",
+		"\\.(gif|ttf|eot|svg)$": "<rootDir>/.tests/fileMock.js",
     '^@components(.*)': '<rootDir>/src/components$1',
     '^@contexts(.*)': '<rootDir>/src/contexts$1',
 	}
---
+++ src/tests.tsx
@@ -0,0 +1,49 @@
+import '@testing-library/jest-dom'
+import { App } from './app'
+import { ITask } from './interfaces/task'
+import { render, screen } from '@testing-library/react'
+import { TodosContext } from '@contexts'
+
+describe('Task\'s existence tests', () => {
+  function renderComponent(tasks: ITask[]) {
+    render(
+      <TodosContext.Provider
+        value={{
+        createTask(content: string) {},
+        deleteTask(taskId: string) {},
+        getDoneTasksCount: () => 0,
+        toggleTaskDone(taskId: string) {},
+        tasks
+        }}
+      >
+        <App />
+      </TodosContext.Provider>
+    )
+  }
+
+  it('shouldn\'t render any task if there aren\'t tasks', () => {
+    // arrange
+    renderComponent([])
+
+    // act
+    const text = screen.getByText('Você ainda não tem tarefas cadastradas')
+  
+    // assert
+    expect(text).toBeInTheDocument()
+  })
+  
+  it('should render tasks if there are tasks', () => {
+    // arrange
+    renderComponent([{
+      id: '0',
+      content: 'Some task to be done',
+      isDone: false
+    }])
+  
+    // act
+    const text = screen.getByText('Some task to be done')
+  
+    // assert
+    expect(text).toBeInTheDocument()
+  })  
+})
