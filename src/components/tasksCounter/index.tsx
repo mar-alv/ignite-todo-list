9265a8d02ac87c374a1d6282585000d4a72367e3
mar-alv <mar.alv.cod@gmail.com>
2024-03-26 21:23:46 -0300

    Issue #5 - tasks counter tests

--- src/components/tasksCounter/index.tsx
+++ src/components/tasksCounter/index.tsx
@@ -19,7 +19,7 @@ export function TasksCounter() {
 
   return (
     <StyledTasksCounter>
-      <CreatedTasks>
+      <CreatedTasks data-testid='created-tasks'>
         Tarefas criadas
 
         <Counter>
@@ -27,7 +27,7 @@ export function TasksCounter() {
         </Counter>
       </CreatedTasks>
 
-      <TasksDone>
+      <TasksDone data-testid='done-tasks'>
         Concluídas
 
         <Counter>
