723516075a3cc9f90021ff649820ec59632b868e
mar-alv <mar.alv.cod@gmail.com>
2024-03-26 21:28:09 -0300

    Issue #5 - delete task tests

--- src/tests.tsx
+++ src/tests.tsx
@@ -133,4 +133,65 @@ describe('Task creation tests', () => {
   })
 })
 
-// TODO: Test deleting a task
+describe('Task deletion tests', () => {
+  function renderComponent(tasks: ITask[]) {
+    return render(
+      <TodosContext.Provider
+        value={{
+          createTask(content: string) {},
+          deleteTask(taskId: string) {},
+          getDoneTasksCount: () => 0,
+          toggleTaskDone(taskId: string) {},
+          tasks
+        }}
+      >
+        <App />
+      </TodosContext.Provider>
+    )
+  }
+
+  it('should delete a task', () => {
+    // arrange
+    const { rerender } = renderComponent([
+      {
+        id: '0',
+        content: 'Some task to be done',
+        isDone: true
+      }
+    ])
+
+    // act
+    const createTaskButton = screen.getByTestId('delete-task-button')
+    fireEvent.click(createTaskButton)
+
+    rerender(
+      <TodosContext.Provider
+        value={{
+          createTask(content: string) {},
+          deleteTask(taskId: string) {},
+          getDoneTasksCount: () => 0,
+          toggleTaskDone(taskId: string) {},
+          tasks: []
+        }}
+      >
+        <App />
+      </TodosContext.Provider>
+    )
+
+    const checkbox = screen.queryByText('Some task to be done')
+
+    // assert
+    expect(checkbox).toBeNull()
+  })
+
+  it('should\'t delete a task if there are no tasks', () => {
+    // arrange
+    renderComponent([])
+
+    // act
+    const createTaskButton = screen.queryByTestId('delete-task-button')
+
+    // assert
+    expect(createTaskButton).toBeNull()
+  })
+})
